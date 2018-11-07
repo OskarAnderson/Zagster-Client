cbcb3a389882bf7e1db9e7582de261484bc97d94
Oskar Anderson <anderosk@oregonstate.edu>
2018-11-07 14:47:54 -0800

    added ability to update total rides

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const Base_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
 
 function updateView() {
-    alert("Beans!")
+    $.getJSON(Base_URL + "/rides/count" , updateRidecount)
+}
+
+function updateRidecount(data) { 
+    numberofRides = data.count
+    $("h2#ridecount").html(numberofRides)
 }
