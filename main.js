403b0833a2d5b49f309b5dc4353a69c9a6070fe1
Oskar Anderson <anderosk@oregonstate.edu>
2018-11-28 00:20:54 -0800

    Removed image, updated chart to grab data in real time, added styles

--- main.js
+++ main.js
@@ -1,46 +1,14 @@
-const Base_URL = "https://zagster-service.herokuapp.com"
-
-const STATIONS = {
-    'columbia_simpson': {
-      latitudeRange: {min: 44.048745, max: 44.049205},
-      longitudeRange: {min: -121.32598, max: -121.32534}
-    },
-    'drake_park': {
-      latitudeRange: {min: 44.058705, max: 44.059165},
-      longitudeRange: {min: -121.31667, max: -121.31603 }
-    },
-    'g5': {
-      latitudeRange: {min: 44.05733, max: 44.05779},
-      longitudeRange: {min: -121.31425, max: -121.31361}
-    },
-    'galveston': {
-      latitudeRange: {min: 44.056495, max: 44.056955},
-      longitudeRange: {min: -121.328415, max: -121.327775}
-    },
-    'grc': {
-      latitudeRange: {min: 44.04412, max: 44.04458},
-      longitudeRange: {min: -121.324075, max: -121.323435}
-    },
-    'old_mill': {
-      latitudeRange: {min: 44.04351, max: 44.04397},
-      longitudeRange: {min: -121.316145, max: -121.315505}
-    },
-    'osu_cascades': {
-      latitudeRange: {min: 44.04276, max: 44.04322},
-      longitudeRange: {min: -121.333995, max: -121.333355}
-    },
-    'ten_barrel': {
-      latitudeRange: {min: 44.060185, max: 44.060645},
-      longitudeRange: {min: -121.313305 , max: -121.312665}
-    }
-  }
-
+let Base_URL = newFunction()
 
 $ (updateView)
 
+function newFunction() {
+    return "https://zagster-service.herokuapp.com";
+}
+
 function updateView() {
     $.getJSON(Base_URL + "/rides/count" , (updateRidecount))
-    $.getJSON(Base_URL + "/rides/count/g5", (updateridecountstation))
+    $.getJSON(Base_URL + "/rides/count/per_year", displayridecountperyear)
 
 }
 
@@ -49,13 +17,11 @@ function updateRidecount(data) {
     $("h2#ridecount").html(numberofRides)
     console.log(data)
 }
-function updateridecountstation(data) {
+function displayridecountperyear(data) {
     numberofrides = data.count
-    $("h3#ridecountstation").html(numberofrides)
+    $("h3#ridecountperyear").html(numberofrides)
     console.log(data)
 }
-
-
 
 
 
